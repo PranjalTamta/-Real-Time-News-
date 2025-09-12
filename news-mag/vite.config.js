f0829a7811de6be3d084768961d1ec01ccbaca03
Pranjal Tamta <pranjaltamta@gmail.com>
2025-09-12 15:03:34 +0530

    another commit

--- news-mag/vite.config.js
+++ news-mag/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // ✅ Important for Vercel
+  base: "/", // use '/' instead of './' for Vercel
 });
